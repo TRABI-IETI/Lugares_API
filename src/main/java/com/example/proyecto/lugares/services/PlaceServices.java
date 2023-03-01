7d9204290c7a037d2f0669df648d4f15af1e99ba
judibec <juanchopolo6954@gmail.com>
2023-03-01 12:47:51 -0500

    finished getPlaces

--- src/main/java/com/example/proyecto/lugares/services/PlaceServices.java
+++ src/main/java/com/example/proyecto/lugares/services/PlaceServices.java
@@ -1,4 +1,10 @@
 package com.example.proyecto.lugares.services;
 
+import com.example.proyecto.lugares.model.Place;
+
+import java.util.List;
+
 public interface PlaceServices {
+
+    List<Place> getAllPlaces();
 }
--- src/main/java/com/example/proyecto/lugares/services/impl/PlaceServicesImpl.java
+++ src/main/java/com/example/proyecto/lugares/services/impl/PlaceServicesImpl.java
@@ -1,6 +1,21 @@
 package com.example.proyecto.lugares.services.impl;
 
+import com.example.proyecto.lugares.model.Place;
 import com.example.proyecto.lugares.services.PlaceServices;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
 public class PlaceServicesImpl implements PlaceServices {
+
+    Map<String, Place> places = new HashMap<>();
+
+    @Override
+    public List<Place> getAllPlaces() {
+        return new ArrayList<>(places.values());
+    }
 }
